6679f9b84076f36413ed5a51285ad0e3a738e697
Matheus Pires <matheushenriquepires99@gmail.com>
2020-11-28 13:29:20 -0300

    refactor: refactor capitalzieFirstLetter return way

--- server/src/utils/capitalizeFirstLetter.ts
+++ server/src/utils/capitalizeFirstLetter.ts
@@ -1,5 +1,4 @@
-const capitalizeFirstLetter = (string: string): string => {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-};
+const capitalizeFirstLetter = (string: string): string =>
+  string.charAt(0).toUpperCase() + string.slice(1);
 
 export default capitalizeFirstLetter;
