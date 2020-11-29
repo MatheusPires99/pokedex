ab4d648cc31ca3c3fa57df4a9a402a5912e0d5af
Matheus Pires <matheushenriquepires99@gmail.com>
2020-11-29 15:12:17 -0300

    feat(mobile): create pokemon type

---
+++ mobile/src/types/pokemon.ts
@@ -0,0 +1,51 @@
+export type Type = {
+  name: string;
+  url: string;
+};
+
+export type Stat = {
+  base_stat: number;
+  name: string;
+  url: string;
+};
+
+export type Ability = {
+  name: string;
+  url: string;
+};
+
+export type Evolution = {
+  first_evolution?: {
+    name: string;
+    url: string;
+    min_level: number;
+    image: string;
+  };
+  second_evolution?: {
+    name: string;
+    url: string;
+    min_level: number;
+    image: string;
+  };
+};
+
+export type EggGroup = {
+  name: string;
+  url: string;
+};
+
+export type Pokemon = {
+  name: string;
+  description: string;
+  image: string;
+  genera: string;
+  pokedex_number: string;
+  base_experience: number;
+  types: Type[];
+  stats: Stat[];
+  height: number;
+  weight: number;
+  abilites: Ability[];
+  evolution?: Evolution[];
+  egg_groups: EggGroup[];
+};
