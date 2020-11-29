f427d6e7c89cd524cb8588317d5d669297b50c7d
Matheus Pires <matheushenriquepires99@gmail.com>
2020-11-29 11:46:55 -0300

    feat(mobile): expo fonts and status bar configuration

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import {
+  Montserrat_400Regular,
+  Montserrat_600SemiBold,
+  Montserrat_700Bold,
+  useFonts,
+} from '@expo-google-fonts/montserrat';
+import { AppLoading } from 'expo';
+import { StatusBar } from 'expo-status-bar';
 
 const App = () => {
+  const [fontsLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_600SemiBold,
+    Montserrat_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Hello World</Text>
-    </View>
+    <>
+      <StatusBar style="dark" backgroundColor="#FFF" translucent />
+
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text style={{ fontSize: 24, fontFamily: 'Montserrat_700Bold' }}>
+          Hello World
+        </Text>
+      </View>
+    </>
   );
 };
 
