2af6162f1a01540615669923fafeebaac8b0e517
Matheus Pires <matheushenriquepires99@gmail.com>
2020-12-03 20:50:52 -0300

    refactor(mobile): delete api-list-result types and change type colors

--- mobile/src/constants/index.ts
+++ mobile/src/constants/index.ts
@@ -9,7 +9,7 @@ export const POKEMON_TYPE_COLORS = {
   ghost: '#705898',
   steel: '#B8B8D0',
   fire: '#FA6C6C',
-  water: '#76BDFE',
+  water: '#6890F0',
   grass: '#48CFB2',
   electric: '#FFCE4B',
   psychic: '#F85888',
@@ -22,3 +22,5 @@ export const POKEMON_TYPE_COLORS = {
 export const HEADER_HEIGHT = 64;
 
 export const POKEMON_SUMMARY_HEIGHT = 360;
+
+export const API_OFFSET = 24;
