8b1dd780db0ae1863af26d772c89aae029148091
Matheus Pires <matheushenriquepires99@gmail.com>
2020-12-08 20:24:41 -0300

    style(mobile): remove flex and position absolute from Container

--- mobile/src/pages/Pokemon/PokemonDetails/styles.ts
+++ mobile/src/pages/Pokemon/PokemonDetails/styles.ts
@@ -7,19 +7,11 @@ import { HEADER_HEIGHT } from '../../../constants';
 const { height } = Dimensions.get('window');
 
 export const Container = styled(Animated.View)`
-  flex: 1;
-
+  height: ${height - (Constants.statusBarHeight + HEADER_HEIGHT)}px;
   background: ${({ theme }) => theme.colors.white};
   border-top-left-radius: 32px;
   border-top-right-radius: 32px;
   padding: 16px 0;
-
-  height: ${height - (Constants.statusBarHeight + HEADER_HEIGHT)}px;
-
-  position: absolute;
-  top: 0;
-  right: 0;
-  left: 0;
 `;
 
 export const TabWrapper = styled.View`
