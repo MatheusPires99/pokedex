85f912eeb5bd69ae7bef13d1ab8123591985e2e3
Matheus Pires <matheushenriquepires99@gmail.com>
2020-12-09 18:24:27 -0300

    fix(mobile): remove stats animation

--- mobile/src/pages/Pokemon/Details/BaseStats/index.tsx
+++ mobile/src/pages/Pokemon/Details/BaseStats/index.tsx
@@ -1,6 +1,4 @@
-import React, { useMemo } from 'react';
-import { useFocusEffect } from '@react-navigation/native';
-import { Animated } from 'react-native';
+import React from 'react';
 
 import Text from '../../../../components/Text';
 import { SlideProps } from '../tabs';
@@ -9,16 +7,6 @@ import Effectiveness from './Effectiveness';
 import { Stat, StatGraph, StatLine, StatValue } from './styles';
 
 const BaseStats = ({ pokemon }: SlideProps) => {
-  const translateX = useMemo(() => new Animated.Value(-1), []);
-
-  useFocusEffect(() =>
-    Animated.timing(translateX, {
-      toValue: 0,
-      duration: 500,
-      useNativeDriver: true,
-    }).start(),
-  );
-
   return (
     <>
       {pokemon.stats.map(stat => (
@@ -33,20 +21,7 @@ const BaseStats = ({ pokemon }: SlideProps) => {
             </Text>
 
             <StatLine>
-              <StatValue
-                width={stat.base_stat}
-                style={{
-                  transform: [
-                    {
-                      translateX: translateX.interpolate({
-                        inputRange: [-1, 0],
-                        outputRange: [-stat.base_stat * 2.5, 0],
-                        extrapolate: 'clamp',
-                      }),
-                    },
-                  ],
-                }}
-              />
+              <StatValue width={stat.base_stat} />
             </StatLine>
           </StatGraph>
         </Stat>
