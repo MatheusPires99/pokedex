c3b63ddaa566c212931de47dc3d3ec061a7e9eb8
Matheus Pires <matheushenriquepires99@gmail.com>
2020-12-09 18:54:22 -0300

    lint(mobile): disable eslint rule

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -60,6 +60,7 @@ const Home = () => {
 
   useEffect(() => {
     loadPokemons();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const refreshList = useCallback(async () => {
