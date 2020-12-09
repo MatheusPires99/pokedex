d24ce9be09c77973170d740621be753ee9c9624c
Matheus Pires <matheushenriquepires99@gmail.com>
2020-12-09 14:38:31 -0300

    feat(mobile): change top-tabs to scollview

--- mobile/src/pages/Pokemon/PokemonDetails/index.tsx
+++ mobile/src/pages/Pokemon/PokemonDetails/index.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import { Animated } from 'react-native';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import { useTheme } from 'styled-components';
+import React, { useCallback, useRef, useState, useMemo } from 'react';
+import { Animated, Dimensions, ScrollView } from 'react-native';
 
 import { POKEMON_SUMMARY_HEIGHT } from '../../../constants';
 import { Pokemon } from '../../../types';
 import Text from '../../../components/Text';
 
-import tabs from './tabs';
-import { Container, TabWrapper } from './styles';
-
-const Tab = createMaterialTopTabNavigator();
+import { tabs } from './tabs';
+import {
+  Container,
+  Tabs,
+  TabButton,
+  SelectedIndicator,
+  SlideWrapper,
+} from './styles';
 
 type PokemonDetailsProps = {
   translateY: Animated.Value;
   pokemon: Pokemon;
 };
 
+const { width } = Dimensions.get('window');
+
+export const TAB_BUTTON_WIDTH = (width - 48) / 4;
+
 const PokemonDetails = ({ pokemon, translateY }: PokemonDetailsProps) => {
-  const { colors } = useTheme();
+  const scrollViewRef = useRef<ScrollView>(null);
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const translateX = useMemo(() => new Animated.Value(0), []);
+
+  const handleChangeTab = useCallback((index: number) => {
+    if (scrollViewRef.current) {
+      scrollViewRef.current.scrollTo({
+        x: width * index,
+        animated: true,
+      });
+
+      setCurrentIndex(index);
+    }
+  }, []);
+
+  const onScroll = Animated.event(
+    [
+      {
+        nativeEvent: {
+          contentOffset: {
+            x: translateX,
+          },
+        },
+      },
+    ],
+    { useNativeDriver: true },
+  );
 
   const containerStyle = {
     transform: [
@@ -32,47 +66,51 @@ const PokemonDetails = ({ pokemon, translateY }: PokemonDetailsProps) => {
     ],
   };
 
+  const selectedIndicatorStyle = {
+    transform: [
+      {
+        translateX: translateX.interpolate({
+          inputRange: tabs.map((_, index) => width * index),
+          outputRange: tabs.map((_, index) => TAB_BUTTON_WIDTH * index),
+          extrapolate: 'clamp',
+        }),
+      },
+    ],
+  };
+
   return (
     <Container style={containerStyle}>
-      <Tab.Navigator
-        backBehavior="none"
-        tabBarOptions={{
-          indicatorStyle: {
-            backgroundColor: colors.blue,
-          },
-          style: {
-            marginHorizontal: 24,
-          },
-        }}
-      >
+      <Tabs>
         {tabs.map((tab, index) => {
-          const { name, component: TabComponent } = tab;
+          const isSelected = index === currentIndex;
 
           return (
-            <Tab.Screen
-              key={index}
-              name={name}
-              children={() => (
-                <TabWrapper>
-                  <TabComponent pokemon={pokemon} />
-                </TabWrapper>
-              )}
-              options={{
-                tabBarLabel: ({ focused }) => (
-                  <Text
-                    color={focused ? 'black' : 'grey'}
-                    bold={!!focused}
-                    numberOfLines={1}
-                    adjustsFontSizeToFit
-                  >
-                    {name}
-                  </Text>
-                ),
-              }}
-            />
+            <TabButton key={index} onPress={() => handleChangeTab(index)}>
+              <Text color={isSelected ? 'black' : 'grey'} bold={isSelected}>
+                {tab.name}
+              </Text>
+            </TabButton>
           );
         })}
-      </Tab.Navigator>
+
+        <SelectedIndicator style={selectedIndicatorStyle} />
+      </Tabs>
+
+      <Animated.ScrollView
+        ref={scrollViewRef}
+        onScroll={onScroll}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        snapToInterval={width}
+        decelerationRate="fast"
+        bounces={false}
+      >
+        {tabs.map(({ slide: Slide }, index) => (
+          <SlideWrapper key={index}>
+            <Slide pokemon={pokemon} />
+          </SlideWrapper>
+        ))}
+      </Animated.ScrollView>
     </Container>
   );
 };
--- mobile/src/pages/Pokemon/PokemonDetails/styles.ts
+++ mobile/src/pages/Pokemon/PokemonDetails/styles.ts
@@ -4,7 +4,9 @@ import Constants from 'expo-constants';
 
 import { HEADER_HEIGHT } from '../../../constants';
 
-const { height } = Dimensions.get('window');
+import { TAB_BUTTON_WIDTH } from '.';
+
+const { height, width } = Dimensions.get('window');
 
 export const Container = styled(Animated.View)`
   height: ${height - (Constants.statusBarHeight + HEADER_HEIGHT)}px;
@@ -14,8 +16,37 @@ export const Container = styled(Animated.View)`
   padding: 16px 0;
 `;
 
-export const TabWrapper = styled.View`
-  flex: 1;
-  background: ${({ theme }) => theme.colors.white};
-  padding: 32px 24px;
+export const Tabs = styled.View`
+  padding: 16px 0 24px;
+  margin: 0 24px;
+  border-bottom-width: 1px;
+  border-style: solid;
+  border-color: ${({ theme }) => theme.colors.lightGrey};
+  position: relative;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+export const TabButton = styled.TouchableOpacity`
+  height: 24px;
+  width: ${TAB_BUTTON_WIDTH}px;
+
+  align-items: center;
+  justify-content: center;
+`;
+
+export const SelectedIndicator = styled(Animated.View)`
+  height: 2px;
+  width: ${TAB_BUTTON_WIDTH}px;
+  background: ${({ theme }) => theme.colors.blue};
+
+  position: absolute;
+  bottom: -1px;
+`;
+
+export const SlideWrapper = styled.View`
+  width: ${width}px;
+  padding: 24px;
 `;
