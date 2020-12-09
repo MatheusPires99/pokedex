63be7a5f3bdfe1f4fe0a3620f44995c2f027de35
Matheus Pires <matheushenriquepires99@gmail.com>
2020-12-09 18:05:37 -0300

    feat(mobile): change TextProps to extend Animated.Props

--- mobile/src/components/Text/index.tsx
+++ mobile/src/components/Text/index.tsx
@@ -1,12 +1,12 @@
 import React, { PropsWithChildren } from 'react';
-import { TextProps as ReactNativeTextProps } from 'react-native';
+import { Animated, TextProps as RNTextProps } from 'react-native';
 
 import { Theme } from '../../styles/styled';
 import theme from '../../styles/theme';
 
 import { Container } from './styles';
 
-export type TextProps = ReactNativeTextProps & {
+export type TextProps = Animated.AnimatedProps<RNTextProps> & {
   variant?: keyof Theme['textVariantes'];
   color?: keyof Theme['colors'];
   bold?: boolean;
