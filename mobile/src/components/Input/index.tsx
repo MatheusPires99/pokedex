615fbe91c3a9adac576dedf14623b560d9d82984
Matheus Pires <matheushenriquepires99@gmail.com>
2020-12-11 14:34:24 -0300

    refactor(mobile): remove onChange TextInput prop to onChangeText

--- mobile/src/components/Input/index.tsx
+++ mobile/src/components/Input/index.tsx
@@ -1,9 +1,5 @@
-import React, { useCallback } from 'react';
-import {
-  TextInputProps,
-  NativeSyntheticEvent,
-  TextInputChangeEventData,
-} from 'react-native';
+import React from 'react';
+import { TextInputProps } from 'react-native';
 import { useTheme } from 'styled-components';
 
 import { Container, Icon, TextInput } from './styles';
@@ -16,15 +12,6 @@ type InputProps = TextInputProps & {
 const Input = ({ setValue, icon, ...rest }: InputProps) => {
   const { colors } = useTheme();
 
-  const onChange = useCallback(
-    (e: NativeSyntheticEvent<TextInputChangeEventData>) => {
-      const value = e.nativeEvent.text;
-
-      setValue(value);
-    },
-    [setValue],
-  );
-
   return (
     <Container>
       <Icon name={icon} size={24} color={colors.black} />
@@ -32,7 +19,7 @@ const Input = ({ setValue, icon, ...rest }: InputProps) => {
       <TextInput
         keyboardAppearance="light"
         placeholderTextColor={`${colors.black}75`}
-        onChange={onChange}
+        onChangeText={setValue}
         {...rest}
       />
     </Container>
