64ef00a56626601965dfe25c9233ac0712ef2b0d
Matheus Pires <matheushenriquepires99@gmail.com>
2020-12-11 11:22:18 -0300

    style(mobile): add semiGrey color, regular font and input text variant

--- mobile/src/styles/theme.ts
+++ mobile/src/styles/theme.ts
@@ -1,15 +1,17 @@
 export default {
   colors: {
     black: '#272727',
+    white: '#FFFFFF',
     grey: '#919191',
     lightGrey: '#E7E7E8',
-    white: '#FFFFFF',
+    semiGrey: '#F4F5F4',
     red: '#FA6555',
     green: '#41D168',
     blue: '#0055D4',
     lilac: '#6C79DB',
   },
   fontFamily: {
+    regular: 'Inter_400Regular',
     medium: 'Inter_500Medium',
     bold: 'Inter_700Bold',
   },
@@ -32,6 +34,11 @@ export default {
     body3: {
       fontSize: '14px',
       fontFamily: 'Inter_500Medium',
+      lineHeight: '18px',
+    },
+    input: {
+      fontSize: '14px',
+      fontFamily: 'Inter_400Regular',
       lineHeight: '18px',
     },
     caption: {
