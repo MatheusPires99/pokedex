b6c5043f91055d9ba9b8a9d9808766d56bf3a420
Matheus Pires <matheushenriquepires99@gmail.com>
2020-12-11 11:25:59 -0300

    feat(mobile): change FlaotingButton from TouchableOpacity to RectButton

--- mobile/src/pages/Home/FloatingButton/styles.ts
+++ mobile/src/pages/Home/FloatingButton/styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components/native';
 import { Animated } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   z-index: 5;
@@ -11,12 +12,16 @@ export const Container = styled.View`
   align-items: flex-end;
 `;
 
-export const Button = styled.TouchableOpacity`
+export const Button = styled(RectButton)`
   width: 56px;
   height: 56px;
-  border-radius: 32px;
+  border-radius: 28px;
   background: ${({ theme }) => theme.colors.lilac};
   box-shadow: 0px 3px 6px ${({ theme }) => `${theme.colors.black}40`};
+
+  position: absolute;
+  bottom: 0;
+  right: 0;
 
   justify-content: center;
   align-items: center;
