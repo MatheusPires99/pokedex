75d3b979c84f69d1fb52e2abf40f01a66c8638d4
Matheus Pires <matheushenriquepires99@gmail.com>
2020-12-12 11:57:10 -0300

    feat(mobile): create api request function and states

--- mobile/src/hooks/search.tsx
+++ mobile/src/hooks/search.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 import React, {
   createContext,
   useCallback,
@@ -6,22 +7,59 @@ import React, {
   PropsWithChildren,
 } from 'react';
 
+import api from '../services/api';
+import { Pokemon } from '../types';
+
 interface SearchContextData {
   isSearching: boolean;
   handleToggleSearch: () => void;
+  searchValue: string;
+  setSearchValue: (value: string) => void;
+  handleSearchPokemon: (pokemon_name: string) => Promise<Pokemon | undefined>;
+  loading: boolean;
 }
 
 const SearchContext = createContext<SearchContextData>({} as SearchContextData);
 
 export const SearchProvider = ({ children }: PropsWithChildren<unknown>) => {
   const [isSearching, setIsSearching] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
+  const [loading, setLoading] = useState(false);
 
   const handleToggleSearch = useCallback(() => {
     setIsSearching(!isSearching);
   }, [isSearching]);
 
+  const handleSearchPokemon = useCallback(async (pokemon_name: string) => {
+    try {
+      setLoading(true);
+
+      const pokemonNameInLowerCase = pokemon_name.toLowerCase();
+
+      const { data: pokemon } = await api.get<Pokemon>(
+        `pokemons/${pokemonNameInLowerCase}`,
+      );
+
+      return pokemon;
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
   return (
-    <SearchContext.Provider value={{ isSearching, handleToggleSearch }}>
+    <SearchContext.Provider
+      value={{
+        isSearching,
+        handleToggleSearch,
+        searchValue,
+        setSearchValue,
+        handleSearchPokemon,
+
+        loading,
+      }}
+    >
       {children}
     </SearchContext.Provider>
   );
