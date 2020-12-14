0ac87bd7fb2fe0b74dea8ba50c1a2f6de997c89f
Matheus Pires <matheushenriquepires99@gmail.com>
2020-12-14 08:13:48 -0300

    feat(mobile): create favorite pokemons context

---
+++ mobile/src/hooks/favoritePokemons.tsx
@@ -0,0 +1,57 @@
+/* eslint-disable consistent-return */
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, {
+  createContext,
+  useCallback,
+  useState,
+  useContext,
+  PropsWithChildren,
+} from 'react';
+import { Alert } from 'react-native';
+
+import { Pokemon } from '../types';
+
+interface FavoritePokemonsContextData {
+  favoritePokemons: Pokemon[];
+  handleGetFavoritePokemons: () => void;
+}
+
+const FavoritePokemonsContext = createContext<FavoritePokemonsContextData>(
+  {} as FavoritePokemonsContextData,
+);
+
+export const FavoritePokemonsProvider = ({
+  children,
+}: PropsWithChildren<unknown>) => {
+  const [favoritePokemons, setFavoritePokemons] = useState<Pokemon[]>([]);
+
+  const handleGetFavoritePokemons = useCallback(async () => {
+    const pokemons = await AsyncStorage.getItem('@Pokedex:pokemons');
+
+    if (pokemons && pokemons !== '[]') {
+      setFavoritePokemons(JSON.parse(pokemons));
+    } else {
+      Alert.alert(
+        'No Pokémons found',
+        'It looks like you do not have any Pokémons on your favorite list.',
+      );
+    }
+  }, []);
+
+  return (
+    <FavoritePokemonsContext.Provider
+      value={{
+        favoritePokemons,
+        handleGetFavoritePokemons,
+      }}
+    >
+      {children}
+    </FavoritePokemonsContext.Provider>
+  );
+};
+
+export default function useFavoritePokemons(): FavoritePokemonsContextData {
+  const context = useContext(FavoritePokemonsContext);
+
+  return context;
+}
